a89d6c7eb158b4ef5e58bda63302430494530113
manavkhanna63 <85454117+manavkhanna63@users.noreply.github.com>
2025-07-11 14:59:36 +0530

    Add files via upload

---
+++ Trade time/client/src/components/Home.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Home() {
+  return <h1>Welcome to TradeTime – Skill Exchange</h1>;
+}
---
+++ Trade time/client/src/components/Register.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Register() {
+  return <h2>Register Page</h2>;
+}
